ad6da969e86d35d8c0150fb68bf06a8bf9c1e3c5
Mahmoud <bp.mahmoud7@gmail.com>
2023-06-05 18:00:51 +0300

    meta update

--- src/app/head.jsx
+++ src/app/head.jsx
@@ -4,7 +4,11 @@ export default function Head() {
       <title>Google Clone Next js 13</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="Google clone created by Next js 13" />
+      <meta
+        http-equiv="Content-Security-Policy"
+        content="upgrade-insecure-requests"
+      />
       <link rel="icon" href="/favicon.svg" />
     </>
-  )
+  );
 }
