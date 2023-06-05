44f78d521cd12c69fce5553e58e8b9e83e67e469
Mahmoud <bp.mahmoud7@gmail.com>
2023-06-05 18:32:07 +0300

    footer update

--- src/components/CountryLookup.jsx
+++ src/components/CountryLookup.jsx
@@ -1,15 +1,7 @@
-"use client";
-
-import { useEffect, useState } from "react";
-
-export default function CountryLookup() {
-  const [country, setCountry] = useState("United States");
-  useEffect(() => {
-    fetch(
-      `http://api.ipstack.com/check?access_key=${process.env.NEXT_PUBLIC_IP_API_KEY}`
-    )
-      .then((res) => res.json())
-      .then((data) => setCountry(data.country_name));
-  }, []);
-  return <div>{country}</div>;
+export default async function CountryLookup() {
+  const response = await fetch(
+    `http://api.ipstack.com/check?access_key=${process.env.NEXT_PUBLIC_IP_API_KEY}`
+  );
+  const jsonData = await response.json();
+  return jsonData ? jsonData.country_name : <div>United States</div>;
 }
